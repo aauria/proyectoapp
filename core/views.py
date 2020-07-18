c0895f4f3f14157f16a4f243d0db7ddcd733d7c3
aauria <afauria@est.itssg.edu.ec>
2020-07-18 00:30:04 -0500

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,HttpResponse
+
+html_base="""
+    <h1>Mi Primer Menu</h1>
+    <ul>
+        <li> <a href="/">Portada</a>    </li>
+        <li> <a href="/about-me/">Acerca de</a>    </li>
+    </ul>
+"""
+
+
+
+def home(request, plantilla="home.html"):
+    return render(request, plantilla);
+def home(request):
+    html_responsde = "<h1>la pagina de portada</h1>"
+    html_responsde = html_base + html_responsde
+    return HttpResponse(html_responsde);
+def about(request, plantilla="about.html"):
+    return render(request, plantilla);
+def about(request):
+    html_responsde = "<h1>Acerca De </h1>"
+    html_responsde = html_base + html_responsde
+    return HttpResponse(html_responsde);
+
+# Create your views here.
